0da289a7ec7420ec2dd9cbdafe17e487c8fae8fa
sarthak agrawal <sharthak31@gmail.com>
2020-06-08 17:19:19 +0530

    add autoLogin feature

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -24,6 +24,20 @@ private authStatusListener = new Subject<boolean>();
     return this.authStatusListener.asObservable();
   }
 
+  autoAuthUser(){
+    const authInformation = this.getAuthData();
+    if(!authInformation){
+      return;
+    }
+    const now = new Date();
+    const expiresIn = authInformation.expirationDate.getTime() - now.getTime();
+    if(expiresIn>0){
+      this.token = authInformation.token;
+      this.isAuthenticated = true;
+      this.authStatusListener.next(true);
+      this.setAuthTimer(expiresIn/1000);
+    }
+  }
 
   createUser(email:string, password:string){
     const authData:AuthData = {email:email, password:password};
@@ -41,23 +55,51 @@ private authStatusListener = new Subject<boolean>();
         this.token = response.token;
         if(this.token){
           const expiresInDuration = response.expiresIn;
-          this.tokenTimer = setTimeout(()=>{
-            alert('You are been LogOut! Please login again');
-            this.logout();
-          }, expiresInDuration*1000);
-
+          this.tokenTimer =this.setAuthTimer(expiresInDuration);
+          const now = new Date();
+          const expirationDate = new Date(now.getTime() + expiresInDuration*1000);
+          console.log(expirationDate);
+          this.saveAuthData(this.token, expirationDate);
           this.isAuthenticated = true;
           this.authStatusListener.next(true);
           this.router.navigate(['/']);
         }
       })
   }
+  private setAuthTimer(duration:number){
+    console.log("your duration is: " + duration);
+    setTimeout(()=>{
+
+      alert('You are been LogOut! Please login again');
+      this.logout();
+    }, duration*1000);
+  }
   logout(){
     this.isAuthenticated = false;
     this.authStatusListener.next(false);
     this.token = null;
     this.router.navigate(['/']);
+    this.clearAuthData();
     clearTimeout(this.tokenTimer);
   }
 
+  private saveAuthData(token:string, expirationDate:Date){
+    localStorage.setItem("token", token);
+    localStorage.setItem("expiration", expirationDate.toISOString());
+  }
+  private clearAuthData(){
+    localStorage.removeItem("token");
+    localStorage.removeItem("expiration");
+  }
+private getAuthData(){
+    const token = localStorage.getItem("token");
+    const expirationDate = localStorage.getItem("expiration");
+    if(!token || !expirationDate){
+      return;
+    }
+    return{
+      token: token,
+      expirationDate: new Date(expirationDate)
+    };
+}
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -14,6 +14,7 @@ export class HeaderComponent implements OnInit, OnDestroy {
   constructor(private authService:AuthService) { }
 
   ngOnInit(): void {
+    this.userIsAuthenticated = this.authService.getIsAuth();
   this.authListenerSubs = this.authService.getAuthStatusListener().subscribe(isAuthenticated=>{
     this.userIsAuthenticated = isAuthenticated;
     console.log("authenticated status is" + this.userIsAuthenticated);
